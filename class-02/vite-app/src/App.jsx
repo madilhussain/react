5cfd8832b6cf0ac700999c1d2f0c79dfbc21cfa2
madilhussain <adilhussain2917@gmail.com>
2024-11-28 23:51:34 +0500

    some cahanges for practice purpose

--- class-02/vite-app/src/App.jsx
+++ class-02/vite-app/src/App.jsx
@@ -1,7 +1,15 @@
 function App() {
+ let MyVariable = 'Hello!';
   return (
     <>
-      <h1>HELLO WORLD!</h1>
+      <h1>HELLO {MyVariable} WORLD!</h1>
+      {
+        /*
+          {} -> Expression | Evaluative Expression, 
+            * Yaha Hm Puri Javascript Nahi Likhty,
+            * Javascript Ka Final Aecom Jo Evaluate Ho Gaya Hai Wo Likhty Hai. 
+        */
+      }
     </>
   )
 }
