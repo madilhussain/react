0a4753b70d709f4665021f6bf0202dd561f9f0ec
madilhussain <adilhussain1729@gmail.com>
2025-04-26 20:56:18 +0500

    some chages for practice purpose

--- currency-convertor/src/App.jsx
+++ currency-convertor/src/App.jsx
@@ -14,10 +14,12 @@ function App() {
   const options = Object.keys(currencyInfo)
   
   const swap = () => {
-    setFrom(to)
-    setTo(from)
-    setConvertedAmount(amount)
-    setAmount(convertedAmount)
+    setFrom(prevFrom => {
+      setTo(prevFrom);
+      return to;
+    });
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
   }
 
   const convert = () => {
@@ -34,18 +36,19 @@ function App() {
           <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
             <form
               onSubmit={(e) => {
-              e.preventDefault();
+                e.preventDefault();
+                convert();
               }}
             >
               <div className="w-full mb-1">
-                <CurrencyInput
-                  label="From"
-                  amount={amount}
-                  currencyOptions={options}
-                  onCurrencyChange={(currency) => setAmount(amount)}
-                  selectCurrency={from}
-                  onAmmountChange={(amount) => setAmount(amount)}
-                />
+              <CurrencyInput
+                label="From"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setFrom(currency)}  // Ye hona chahiye
+                selectCurrency={from}
+                onAmmountChange={(amount) => setAmount(amount)}
+              />
               </div>
               <div className="relative w-full h-0.5">
                 <button
@@ -57,14 +60,14 @@ function App() {
                 </button>
               </div>
               <div className="w-full mt-1 mb-4">
-                <CurrencyInput
-                  label="To"
-                  amount={convertedAmount}
-                  currencyOptions={options}
-                  onCurrencyChange={(currency) => setTo(currency)}
-                  selectCurrency={from}
-                  amountDisabled
-                />
+              <CurrencyInput
+                label="To"
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}  // Yahan To ka setTo
+                selectCurrency={to}
+                amountDisabled
+              />
               </div>
               <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
                 Convert {from.toUpperCase()} TO {to.toUpperCase()}
