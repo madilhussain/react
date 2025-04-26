18d3c495694a73e0aab8211b82cf63aeba086a03
madilhussain <adilhussain1729@gmail.com>
2025-04-26 22:36:56 +0500

    some chages for practice purpose

---
+++ react-router/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-black text-white font-bold p-4 rounded-2xl'>REACT ROUTER</h1>
+    </>
+  )
+}
+
+export default App
